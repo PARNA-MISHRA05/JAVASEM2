43206db827f7bf1c651973618480607a66140e74
PARNA MISHRA <126265242+PARNA-MISHRA05@users.noreply.github.com>
2023-07-16 18:38:27 +0530

    Update Exam.java

--- Exam.java
+++ Exam.java
@@ -4,7 +4,8 @@ class Parent
 	protected int r;
 	public void accept()
 	{
-		Scanner sc=new Scanner(System.in);		   			System.out.println("ENTER RADIUS: ");
+		Scanner sc=new Scanner(System.in);		   			
+		System.out.println("ENTER RADIUS: ");
 		r=sc.nextInt();
 	}
 }
@@ -13,7 +14,8 @@ class Child extends Parent
 	private int area;
 	public void display()
 	{
-		area=r*r;		   							System.out.println("AREA IS :  "+area);
+		area=r*r;		  
+		System.out.println("AREA IS :  "+area);
 		
 	}
 }
